1cc289ff33cf73cbf88c945fca5946bb392e4715
DuramAsh <duramash.02@gmail.com>
2021-11-17 17:59:39 +0600

    smth

---
+++ Group 3/17.11.2021/1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+
+    stack <int> st;
+    st.push(5);
+    st.push(2);
+    st.push(-4);
+    st.push(12);
+    while(!st.empty()){
+        printf("The size of the stack is: %d\nThe top element is: %d\n", st.size(), st.top());
+        st.pop();
+    }
+
+    return 0;
+}
